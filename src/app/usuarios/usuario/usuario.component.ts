a5c78d481d5104b3f539312afb8c84126be749d0
Hansel <hanselcapillo@hotmail.com>
2020-01-30 15:43:25 -0500

    mostrar el deatella de un usuario

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { AppState } from "src/app/store/app.reducer";
 import { CargarUsuario } from "src/app/store/actions";
+import { Usuario } from "../../models/usuario.model";
 
 @Component({
   selector: "app-usuario",
@@ -10,12 +11,27 @@ import { CargarUsuario } from "src/app/store/actions";
   styleUrls: ["./usuario.component.scss"]
 })
 export class UsuarioComponent implements OnInit {
+  public user: Usuario;
+  public loading: boolean;
+  public error: {
+    status: string;
+    message: string;
+    url: string;
+  };
+
   constructor(private router: ActivatedRoute, private store: Store<AppState>) {}
 
   ngOnInit() {
     this.router.params.subscribe(params => {
       const id = params[`id`];
       this.store.dispatch(new CargarUsuario(id));
+    });
+
+    // obtener la data del usuario
+    this.store.select("usuario").subscribe(usuario => {
+      this.user = usuario.user;
+      this.loading = usuario.loading;
+      this.error = usuario.error;
     });
   }
 }
--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ListaComponent } from "./lista/lista.component";
 import { UsuarioComponent } from "./usuario/usuario.component";
+import { RouterModule } from "@angular/router";
 
 const component = [ListaComponent, UsuarioComponent];
 
 @NgModule({
   declarations: [...component],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [...component]
 })
 export class UsuariosModule {}
